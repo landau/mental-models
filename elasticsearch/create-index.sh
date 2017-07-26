165b2625866d8749da30d7bb40d8613f99649221
Trevor Landau <landautrevor@gmail.com>
2017-07-25 21:36:19 -0400

    add basic scripts

---
+++ elasticsearch/create-index.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+if [[ $# -ne 2 ]]; then
+    printf "Example: $0 http://localhost:9200 models\n"
+    exit 1
+fi
+
+ES_ENDPOINT=$1
+INDEX=$2
+
+printf "Creating index ${INDEX}\n"
+curl -XPUT -d @mappings.json "${ES_ENDPOINT}/${INDEX}"
